9ac0fd66d16af46896af19e4a5493a75b924cd8d
white-ras <rasras3366@gmail.com>
2024-06-14 10:34:42 +0900

    feat: 初級課題１です。

--- 1.go
+++ 1.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	s := []any{1, "2", 10, "11"}
+	var err error
+
+	for _, v := range s {
+		// 出力用
+		var i int
+
+		switch v := v.(type) {
+		case int:
+			i = v
+		case string:
+			i, err = strconv.Atoi(v)
+			if err != nil {
+				fmt.Println(err)
+			}
+		default:
+			fmt.Println("unknown!!!!!")
+		}
+		fmt.Printf("%02d\n", i)
+	}
+}
