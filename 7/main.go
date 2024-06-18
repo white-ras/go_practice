9c547f7f2ee8424161bd1dd7fa4d711ad5ac16de
white-ras <rasras3366@gmail.com>
2024-06-18 16:10:12 +0900

    feat:userテーブルを作成

--- 7/main.go
+++ 7/main.go
@@ -1,2 +1,61 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+const (
+	tableNameUser = "users"
+)
+
+func main() {
+	// .envファイルから環境変数を読み込む
+	err := godotenv.Load()
+	if err != nil {
+		log.Panicln("Error loading .env file")
+	}
+
+	// 環境変数からデータベース接続情報を取得
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+
+	// データベース接続URLを生成
+	dbURL := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=disable",
+		dbUser, dbPassword, dbName, dbHost, dbPort)
+
+	// データベースに接続
+	Db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer Db.Close()
+
+	// データベースに接続できるか確認
+	err = Db.Ping()
+	if err != nil {
+		log.Panicln("Database ping failed:", err)
+	}
+	log.Println("Successfully connected to the database")
+
+	// テーブルを作成
+	cmdT := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
+		id SERIAL PRIMARY KEY,
+		age INTEGER,
+		name VARCHAR(500),
+		role CHAR(15))`, tableNameUser)
+	_, err = Db.Exec(cmdT)
+	if err != nil {
+		log.Panicln("Error creating table:", err)
+	}
+
+	log.Println("Table 'users' created successfully")
+}
